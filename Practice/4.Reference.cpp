98154503bd341221e39a44c5f2d31d9e722a0f03
Ayush <91123494+soul-999-ayu@users.noreply.github.com>
2023-10-08 22:06:02 +0530

    Reference practice

---
+++ Practice/4.Reference.cpp
@@ -0,0 +1,28 @@
+//Reference practice code
+#include <iostream>
+
+using namespace std;
+
+void change(int &c, int value){
+    c=value;
+}
+
+int main() {
+    int a = 10;
+    int &r = a;
+    
+    //Before changing value
+    cout << "Value before changing:" << endl;
+    cout << "Value of a " << a << endl;
+    cout << "Value of a (using reference r): " << r << endl;
+    
+    //Changing value using reference
+    change(a, 69);
+    
+    //After changing value
+    cout << endl << "Value after changing:" << endl;
+    cout << "Value of a " << a << endl;
+    cout << "Value of a (using reference r): " << r << endl;
+    
+    return 0;
+}
