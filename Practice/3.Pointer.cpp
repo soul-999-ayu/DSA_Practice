345d0ff60bf068cd93ff6f892c6bf990e21fa164
Ayush <91123494+soul-999-ayu@users.noreply.github.com>
2023-10-08 22:24:23 +0530

    Added some fixes and more code

--- Practice/3.Pointer.cpp
+++ Practice/3.Pointer.cpp
@@ -5,23 +5,24 @@
 using namespace std;
 
 int main() {
+    //Creating variable and pointer
     int a = 10;
     int *p = &a;
     
+    //Printing values
     cout << "Value of a " << a << endl;
     cout << "Value of a (using pointer p): " << *p << endl;
     cout << "Address of a: " << &a << endl;
     cout << "Address of a (using pointer p): " << p << endl;
     
-    //C Language
+    //C Code
     int *p1 = (int *) malloc(sizeof(int)); //Allocating memory
     *p1=100; //Assigning value
+    cout << endl << "Value of p1: " << *p1 << endl;
     
-    //C++ Language
+    //C++ Code
     int *p2 = new int; //Allocating memory
     *p2=200; //Assigning value
-    
-    cout << endl << "Value of p1: " << *p1 << endl;
     cout << "Value of p2: " << *p2 << endl;
     
     //Array example
@@ -32,6 +33,10 @@ int main() {
         cout << endl << "Array value at array[" << i << "]: " << array[i];
         cout << endl << "Array value at arrayR[" << i << "]: " << arrayP[i] << endl;
     }
+
+    //Deallocating memory
+    free(p1);
+    delete(p2);
 
     return 0;
 }
