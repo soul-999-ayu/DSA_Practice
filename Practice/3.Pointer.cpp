739e28dc947e5603f8a9e5f8d0037f961b0292bc
Ayush <91123494+soul-999-ayu@users.noreply.github.com>
2023-10-08 22:05:27 +0530

    Added array example

--- Practice/3.Pointer.cpp
+++ Practice/3.Pointer.cpp
@@ -17,12 +17,21 @@ int main() {
     int *p1 = (int *) malloc(sizeof(int)); //Allocating memory
     *p1=100; //Assigning value
     
-     //C++ Language
+    //C++ Language
     int *p2 = new int; //Allocating memory
     *p2=200; //Assigning value
     
     cout << endl << "Value of p1: " << *p1 << endl;
     cout << "Value of p2: " << *p2 << endl;
+    
+    //Array example
+    int array[5] = {10, 20, 30, 40, 50};
+    int *arrayP = array;
+    
+    for(int i=0; i<5; i++){
+        cout << endl << "Array value at array[" << i << "]: " << array[i];
+        cout << endl << "Array value at arrayR[" << i << "]: " << arrayP[i] << endl;
+    }
 
     return 0;
 }
