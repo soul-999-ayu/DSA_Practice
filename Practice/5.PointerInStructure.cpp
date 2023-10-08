7eace7d17dc5bf2020aa1f9049e612b65895a97c
Ayush <91123494+soul-999-ayu@users.noreply.github.com>
2023-10-08 22:21:23 +0530

    Pointer practice

---
+++ Practice/5.PointerInStructure.cpp
@@ -0,0 +1,42 @@
+//Pointer practice code
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+struct rect{
+    int length;
+    int breadth;
+};
+
+int main() {
+    //Creating variables and pointer
+    struct rect r = {10, 20};
+    struct rect *p = &r;
+    
+    //Printing values
+    cout << "Length: " << r.length << endl;
+    cout << "Length (using pointer): " << p->length << endl;
+    cout << "Breadth: " << r.breadth << endl;
+    cout << "Breadth (using pointer): " << p->breadth << endl;
+    
+    //C Code (Dynamic allocation)
+    struct rect *p1 = (struct rect *)malloc(sizeof(struct rect));
+    p1->length = 69;
+    p1->breadth = 96;
+    cout << endl << "Length of p1: " << p1->length << endl;
+    cout << "Breadth of p1: " << p1->breadth << endl;
+    
+    //C++ Code (Dynamic allocation)
+    struct rect *p2 = new struct rect;
+    p2->length = 999;
+    p2->breadth = 420;
+    cout << endl << "Length of p2: " << p2->length << endl;
+    cout << "Breadth of p2: " << p2->breadth;
+    
+    //Deallocating memory
+    free(p1);
+    delete(p2);
+    
+    return 0;
+}
