935e1d28d3419a5384f8e59460505389d5b5fd5b
Ayush <91123494+soul-999-ayu@users.noreply.github.com>
2023-10-08 21:46:17 +0530

    Pointer practice

---
+++ Practice/3.Pointer.cpp
@@ -0,0 +1,28 @@
+//Pointer practice code
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+    int a = 10;
+    int *p = &a;
+    
+    cout << "Value of a " << a << endl;
+     cout << "Value of a (using pointer p): " << *p << endl;
+    cout << "Address of a: " << &a << endl;
+    cout << "Address of a (using pointer p): " << &a << endl;
+    
+    //C Language
+    int *p1 = (int *) malloc(sizeof(int)); //Allocating memory
+    *p1=100; //Assigning value
+    
+     //C++ Language
+    int *p2 = (int *) malloc(sizeof(int)); //Allocating memory
+    *p2=200; //Assigning value
+    
+    cout << endl << "Value of p1: " << *p1 << endl;
+    cout << "Value of p2: " << *p2 << endl;
+
+    return 0;
+}
